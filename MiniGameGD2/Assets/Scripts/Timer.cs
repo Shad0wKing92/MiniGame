705932b7b606258f8527663333c35d12dbb7edb8
jettraverso <bridgettraverso@gmail.com>
2016-02-17 21:32:18 -0600

    UI
    UI and Menu jazz

--- MiniGameGD2/Assets/Scripts/Timer.cs
+++ MiniGameGD2/Assets/Scripts/Timer.cs
@@ -1,45 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 
 	public int timer = 1000;
 	public bool timerRunning = false;
-
+	public Text timeLabel;
 
 	// Use this for initialization
 	void Start () {
-
+//		timeLabel = GetComponent<Text>();
+//		print (timeLabel.text);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (timerRunning)
-			timer--;
+		timeLabel.text = "Time: " + timer.ToString(); 
 
-		if (timer == 0)
-			timerRunning = false;
+		checkTimer ();
 	}
 
+	void checkTimer(){
+		if (timerRunning)
+			timer--;
+		
+		if (timer == 0) {
+			timerRunning = false;
 
-	void StartTime()
+		}
+	}
+
+	public void StartTime()
 	{
 		timerRunning = true;
 	}
 
-	void OnGUI()
-	{
-		GUI.Label (new Rect (10, 40, 100, 100), ("time: " + timer));
-
-		if(!timerRunning){
-			if(GUI.Button (new Rect(10, 75, 100, 50), ("Reset"))){
-				Application.LoadLevel(0);
-			}
-		}
-		if(timer == 1000){
-			if (GUI.Button (new Rect (10, 125, 100, 50), ("Start"))) {
-				StartTime ();	
-			}
-		}
-	}
+//	public void StartClick(){
+//		StartTime();
+//	}
+//	public void OnClick()
+//	{
+//		GUI.Label (new Rect (10, 40, 100, 100), ("time: " + timer));
+//
+//		if(!timerRunning){
+//			if(){
+//				Application.LoadLevel(0);
+//			}
+//		}
+//		if(timer == 1000){
+//			if (GUI.Button (new Rect (10, 125, 100, 50), ("Start"))) {
+//				StartTime ();	
+//			}
+//		}
+//	}
 }
---
+++ MiniGameGD2/Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ResetClick(){
+		Application.LoadLevel (1);
+	}
+
+	public void StartClick(Timer timer){
+			timer.StartTime();
+	}
+}
